050156f8fd0876b408850bd839608cd85837b52a
Ryan Harter <harterrt@gmail.com>
2016-10-28 15:43:26 -0400

    Summarize pings from "ESR" channel over time

---
+++ main.R
@@ -0,0 +1,20 @@
+require(ggplot2)
+
+getData <- function() {
+  out <- read.csv("Other_Ping_2016_10_28.csv")
+  out$date <- as.Date(substr(out$submission, 1, 10))
+  out$is_esr_ping <- out$channel == "esr"
+
+  out
+}
+
+plotInc <- function(dd = getData()) {
+  ggplot(data=dd, aes(x=date, weight=count, fill=is_esr_ping)) +
+    geom_bar() +
+    theme(axis.text.x = element_text(angle = 270, hjust = 1)) +
+    ylab("Number of Pings in 1% Sample") +
+    xlab("Submission Date")
+}
+
+pp <- plotInc(getData())
+ggsave("other_pings_investigation.png", pp)
